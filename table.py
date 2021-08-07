8a6b66a7aca317751370f0ef86fed4546c1324df
unknown <ebinjacob1999@gmail.com>
2021-08-07 12:04:16 +0530

    mul tables upto

--- table.py
+++ table.py
@@ -3,7 +3,8 @@ while(1):
     n = int(input("Enter a number to get the multiplication table: "))
     if n==-1:
         exit()
-    for i in range(1,11):
+    val = int(input("Print multiplication tables upto: "))
+    for i in range(1,val+1):
         pro = i*n
         print(str(n)+" X "+str(i)+" = "+str(pro))
     
