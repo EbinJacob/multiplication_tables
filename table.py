ee9306bc2079bc42c31bd87a69d8615f26d9908a
unknown <ebinjacob1999@gmail.com>
2021-08-07 12:08:26 +0530

    basic formatting

--- table.py
+++ table.py
@@ -1,10 +1,15 @@
 while(1):
+    print("- - - MULTIPLICATION TABLE GENERATOR - - -")
+    print(" ")
     print("(To exit enter -1)")
     n = int(input("Enter a number to get the multiplication table: "))
     if n==-1:
         exit()
     val = int(input("Print multiplication tables upto: "))
+    print(" ")
     for i in range(1,val+1):
         pro = i*n
         print(str(n)+" X "+str(i)+" = "+str(pro))
+    print(" ")
+    print(" ")
     
